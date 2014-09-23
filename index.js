08c514703d94b5ebea12461358c6d5368e41b2f9
David Glivar <davidglivar@gmail.com>
2014-09-23 15:28:35 -0700

    removed cache plugin, was not playing nicely

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var cache = require('gulp-cache')
-  , imagemin = require('gulp-imagemin');
+var imagemin = require('gulp-imagemin');
 
 module.exports = function (voyager) {
   
   voyager.task('write', 'images', function (done) {
     this.src(['images/**', '!images/sprites/**'])
-      .pipe(cache(imagemin({
+      .pipe(imagemin({
         progressive: true
       , interlaced: true
-      })))
-      .on('error', function (e) { console.log(e) })
+      }))
       .pipe(this.out('images'))
       .on('end', done);
   });
